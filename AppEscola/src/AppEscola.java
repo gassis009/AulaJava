0f7f085de6c52c0a9c096620623d907113a1c1fa
gassis009 <47761244+gassis009@users.noreply.github.com>
2019-02-19 21:56:59 -0300

    Add files via upload

---
+++ AppEscola/src/AppEscola.java
@@ -0,0 +1,17 @@
+
+
+public class AppEscola {
+
+   
+
+
+  
+    public static void main(String[] args) {
+       Pessoa p1 = new Professor ("zezinho");
+       Professor p2 = new Professor ("zezinho");
+       
+       
+       System.out.println(p1.retornarNome());
+    }
+    
+}
